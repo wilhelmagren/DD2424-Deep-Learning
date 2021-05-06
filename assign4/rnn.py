bbb265d414b9d464e74cd14774d4c51f59a82887
Bund <wilhelmagren98@gmail.com>
2021-05-06 13:23:55 +0200

    File loading/writing fixed. Better docs

--- assign4/rnn.py
+++ assign4/rnn.py
@@ -12,14 +12,16 @@ import matplotlib.pyplot as plt
 class RNN:
     def __init__(self, sigma=0.01, m=100, eta=0.1, seq_length=25, verbose=False):
         self.FILEPATH = os.getcwd()
-        self.DATA_FILEPATH = os.path.join(self.FILEPATH, 'data\\goblet_book.txt')
-        self.VOCAB_FILEPATH = os.path.join(self.FILEPATH, 'data\\vocab.p')
+        self.BOOK_FILEPATH = os.path.join(self.FILEPATH, 'data\\goblet_book.txt')
+        self.i2c_FILEPATH = os.path.join(self.FILEPATH, 'data\\vocab.p')
+        self.DATA_FILEPATH = os.path.join(self.FILEPATH, 'data\\data.p')
         self.vocab = set()
         self.book_data = []
         self.i2c = {}
         self.c2i = {}
         self.m = m
         self.K = 0
+        self.N = 0  # After loading/reading data should be set to 1107545 for the given HP book.
         self.eta = eta
         self.sigma = sigma
         self.seq_length = seq_length
@@ -30,6 +32,7 @@ class RNN:
         self.V = np.zeros(1)
         self.h0 = np.zeros(1)
         self.verbose = verbose
+        print('+'*100)
 
     def init_model(self):
         self.b = np.zeros((self.m, 1))
@@ -41,62 +44,60 @@ class RNN:
 
     def prepare_data(self):
         if self.verbose:
-            print(f'<| preparing the data from filepath: {self.DATA_FILEPATH}')
-        if os.path.isfile(self.VOCAB_FILEPATH):
-            self.__load_vocab()
+            print(f'<| preparing the data from filepath: {self.BOOK_FILEPATH}')
+        if os.path.isfile(self.DATA_FILEPATH):
+            self.__load()
+            assert len(self.vocab) == len(self.i2c.keys()), print(f'!!! ERROR :: vocab size not equal i2c size, {len(self.vocab)} != {len(self.i2c.keys())}')
             return
-        with open(self.DATA_FILEPATH, 'r') as fd:
+        with open(self.BOOK_FILEPATH, 'r') as fd:
             for char in fd.read():
+                self.book_data.append(char)
                 if char not in self.vocab:
                     self.vocab.add(char)
                     self.i2c[len(self.vocab) - 1] = char
                     self.c2i[char] = len(self.vocab) - 1
 
         self.K = len(self.vocab)
-        self.__write_vocab()
+        self.N = len(self.book_data)
+        self.__write()
 
-    def __load_vocab(self):
         if self.verbose:
-            print(f'<| vocab already exists, loading it and building idx dictionaries')
-        self.i2c = pickle.load(open(self.VOCAB_FILEPATH, 'rb'))
-        for idx in self.i2c:
-            self.c2i[self.i2c[idx]] = idx
-            self.vocab.add(self.i2c[idx])
-        self.K = len(self.vocab)
-
-    def __write_vocab(self):
-        if self.verbose:
-            print(f'<| writing the vocab to filepath: {self.VOCAB_FILEPATH}')
-        pickle.dump(self.i2c, open(self.VOCAB_FILEPATH, 'wb'))
+            print(f'<| prepared data features below:\n'
+                  f'\t\tsize of vocab:\t{len(self.vocab)}\n'
+                  f'\t\tsize of i2c:\t{len(self.i2c.keys())}\n'
+                  f'\t\tsize of c2i:\t{len(self.c2i.keys())}\n'
+                  f'\t\tsize of data:\t{len(self.book_data)}')
 
     def loss(self, x, y):
         pass
 
-    def forward_pass(self, xt, tao=2):
-        h_l = [self.h0]
-        p_l = []
-        for t in range(1, tao):
-            # (m x 1) = (m x m)*(m x 1) + (m x k)*(d x 1) + (m x 1)   ::   Hidden state at time t before non-linearity
-            at = self.W@h_l[t-1] + self.U@xt + self.b
-            # hidden state at time t of size   (m x 1)
-            h_l.append(np.tanh(at))
-            # output vector of unnormalized log probabilities for each class at time t of size   (K x 1)
-            ot = self.V@h_l[t] + self.c
-            # output probability vector at time t of size   (K x 1)
-            p_l.append(self.softmax(ot))
-        return h_l, p_l
+    def cross_entropy(self, pt, yt):
+        pass
+
+    def forward_pass(self, xt, ht=None):
+        if ht is None:
+            ht = self.h0
+        # (m x 1) = (m x m)*(m x 1) + (m x k)*(d x 1) + (m x 1)   ::   Hidden state at time t before non-linearity
+        at = self.W@ht + self.U@xt + self.b
+        # hidden state at time t of size   (m x 1)
+        ht = np.tanh(at)
+        # output vector of unnormalized log probabilities for each class at time t of size   (K x 1)
+        ot = self.V@ht + self.c
+        # output probability vector at time t of size   (K x 1)
+        pt = self.softmax(ot)
+        return ht, pt
 
     def synthesize(self, x0, n=None):
+        if self.verbose:
+            print(f'<| synthesizing a char sequence of length {n} from initial char {x0}')
         if n is None:
             n = self.seq_length
         if type(x0) is str:
             x0 = self.c2i[x0]
-        if self.verbose:
-            print(f'<| synthesizing a char sequence of length {n} from initial char {x0}')
-        s = ''
+        s = '\t\t'
         for idx in range(n):
-            _, p_l = self.forward_pass(self.onehot_idx(x0), tao=2)
-            rand_idx = np.random.choice(np.arange(0, self.K), p=p_l[0].flatten())
+            ht, pt = self.forward_pass(self.onehot_idx(x0))
+            rand_idx = np.random.choice(np.arange(0, self.K), p=pt.flatten())
             s += self.i2c[x0]
             x0 = rand_idx
         print(s)
@@ -110,13 +111,44 @@ class RNN:
     def softmax(x):
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
+    def __load(self):
+        if self.verbose:
+            print(f'\t\tvocab already exists, loading it and building idx dictionaries ...')
+        self.i2c = pickle.load(open(self.i2c_FILEPATH, 'rb'))
+        for idx in self.i2c:
+            self.c2i[self.i2c[idx]] = idx
+            self.vocab.add(self.i2c[idx])
+        self.K = len(self.vocab)
+        self.book_data = pickle.load(open(self.DATA_FILEPATH, 'rb'))
+        self.N = len(self.book_data)
+        if self.verbose:
+            print(f'<| loaded data features below:\n'
+                  f'\t\tsize of vocab:\t{len(self.vocab)}\n'
+                  f'\t\tsize of i2c:\t{len(self.i2c.keys())}\n'
+                  f'\t\tsize of c2i:\t{len(self.c2i.keys())}\n'
+                  f'\t\tsize of data:\t{len(self.book_data)}')
+
+    def __write(self):
+        if self.verbose:
+            print(f'\t\twriting the i2c to filepath: {self.i2c_FILEPATH}')
+            print(f'\t\twriting the data to filepath: {self.DATA_FILEPATH}')
+        pickle.dump(self.i2c, open(self.i2c_FILEPATH, 'wb'))
+        pickle.dump(self.book_data, open(self.DATA_FILEPATH, 'wb'))
+
+    def __repr__(self):
+        return 'Class(RNN)'
+
+    def __del__(self):
+        print(f'<| deleting instance of {self.__repr__()}')
+        print('+' * 100)
+
 
 def main():
     rnn = RNN(verbose=True)
     rnn.prepare_data()
     rnn.init_model()
     # rnn.forward_pass(tmp_x)
-    rnn.synthesize('.', n=5)
+    rnn.synthesize('k', n=5)
 
 
 if __name__ == '__main__':
