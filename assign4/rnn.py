6ca1bce67651887b48f9d276b15cb6434a015cb6
Bund <wilhelmagren98@gmail.com>
2021-05-10 11:28:14 +0200

    backprop through time implemented. need to test against numerical gradients

--- assign4/rnn.py
+++ assign4/rnn.py
@@ -30,6 +30,9 @@ class RNN:
         self.U = np.zeros(1)
         self.W = np.zeros(1)
         self.V = np.zeros(1)
+        self.grad_U = np.zeros(1)
+        self.grad_W = np.zeros(1)
+        self.grad_V = np.zeros(1)
         self.h0 = np.zeros(1)
         self.x_chars = np.zeros((1, 1))
         self.y_chars = np.zeros((1, 1))
@@ -53,6 +56,9 @@ class RNN:
         self.U = np.random.rand(self.m, self.K)*self.sigma
         self.W = np.random.rand(self.m, self.m)*self.sigma
         self.V = np.random.rand(self.K, self.m)*self.sigma
+        self.grad_U = np.zeros((self.m, self.m))
+        self.grad_W = np.zeros((self.m, self.m))
+        self.grad_V = np.zeros((self.K, self.m))
         self.h0 = np.zeros((self.m, ))
         self.x_chars = np.zeros((self.K, self.seq_length))
         self.y_chars = np.zeros((self.K, self.seq_length))
@@ -91,11 +97,11 @@ class RNN:
             xt = self.x_chars
         if yt is None:
             yt = self.y_chars
-        tao = xt.shape[1]
-        h_l, p_l, a_l, o_l, loss_l = np.zeros((self.m, tao + 1)), np.zeros((self.K, tao)), \
-                                     np.zeros((self.m, tao)), np.zeros((self.K, tao)), []
+        tau = xt.shape[1]
+        h_l, p_l, a_l, o_l, loss_l = np.zeros((self.m, tau + 1)), np.zeros((self.K, tau)), \
+                                     np.zeros((self.m, tau)), np.zeros((self.K, tau)), np.zeros((1, tau))
         h_l[:, 0] = ht
-        for t in range(tao):
+        for t in range(tau):
             # (m x 1) = (m x m)*(m x 1) + (m x k)*(d x 1) + (m x 1)   ::   Hidden state at time t before non-linearity
             a_l[:, t] = self.W@h_l[:, t] + self.U@xt[:, t] + self.b
             # hidden state at time t of size   (m x 1)
@@ -104,14 +110,57 @@ class RNN:
             o_l[:, t] = self.V@h_l[:, t] + self.c
             # output probability vector at time t of size   (K x 1)
             p_l[:, t] = self.softmax(o_l[:, t])
-            loss_l.append(self.loss(p_l[:, t], yt[:, t]))
-        return h_l, p_l, a_l, o_l
+            loss_l[:, t] = self.loss(p_l[:, t], yt[:, t])
+        return h_l, p_l, a_l, o_l, loss_l
 
     def adagrad(self, xt, yt):
-        # grad_ot = -(y-p)^T
-        # grad_pt = -(y^T)/(np.dot(y^t, pt))
-        h_l, p_l, a_l, o_l = self.forward_pass(xt=xt, yt=yt)
-        print(np.array(yt).shape)
+        """
+        Gradient computations for RNN, back propagation through time BPTT.
+            xt      ::  (K x tau)
+            yt      ::  (K x tau)
+            ht      ::  (m x tau + 1)
+            pt      ::  (K x tau)
+            at      ::  (m x tau)
+            ot      ::  (K x tau)
+            loss    ::  (1 x tau)
+        """
+        tau = xt.shape[1]
+        ht, pt, at, ot, loss = self.forward_pass(xt=xt, yt=yt)
+
+        assert ht.shape == (self.m, tau + 1)
+        assert pt.shape == (self.K, tau)
+        assert at.shape == (self.m, tau)
+        assert ot.shape == (self.K, tau)
+
+        grad_o_l = np.zeros((tau, self.K))
+        grad_p_l = np.zeros((tau, self.K))
+        for t in range(tau):
+            # - y_t^T/(y_l^T * p_t)     ::     (1 x K)/(1 x 1)  == (1 x K)
+            grad_p_l[t, :] = -yt[:, t].T/(yt[:, t].T@pt[:, t])
+            # -(y_t - p_t)^T    ::    (K x 1  -  K x 1)^T  ==  (1 x K)
+            grad_o_l[t, :] = -(yt[:, t] - pt[:, t]).T
+
+        grad_V_l = np.zeros((self.K, self.m))
+        for t in range(tau):
+            grad_V_l += np.asmatrix(grad_o_l[t, :]).T@np.asmatrix(ht[:, t + 1])
+
+        grad_h_l = np.zeros((tau, self.m))
+        grad_a_l = np.zeros((tau, self.m))
+        grad_h_l[tau - 1, :] = grad_o_l[tau - 1, :]@self.V  # (1 x m)  =  (1 x K)@(K x m)
+        grad_a_l[tau - 1, :] = grad_h_l[tau - 1, :]@np.diag(1 - (np.tanh(at[:, tau - 1])**2))  # (1 x m)  =  (1 x m)@(np.diag(1 - tanh^2(m x 1))
+        for t in range(tau - 2, -1, -1):
+            grad_h_l[t, :] = grad_o_l[t, :]@self.V + grad_a_l[t + 1, :]@self.W
+            grad_a_l[t, :] = grad_h_l[t, :]@np.diag(1 - (np.tanh(at[:, t])**2))
+
+        grad_W_l = np.zeros((self.m, self.m))
+        grad_U_l = np.zeros((self.m, self.K))
+        for t in range(tau):
+            grad_W_l += np.asmatrix(grad_a_l[t, :]).T@np.asmatrix(ht[:, t])
+            grad_U_l += np.asmatrix(grad_a_l[t, :]).T@np.asmatrix(xt[:, t])
+
+        self.grad_V = grad_V_l
+        self.grad_W = grad_W_l
+        self.grad_U = grad_U_l
 
     def synthesize(self, x0, n=None):
         if self.verbose:
@@ -122,8 +171,8 @@ class RNN:
             x0 = self.c2i[x0]
         s = '\t\t'
         for idx in range(n):
-            _, pt, _, _ = self.forward_pass(xt=self.onehot_idx(x0))
-            rand_idx = np.random.choice(np.arange(0, self.K), p=pt.flatten())
+            _, pt, _, _, _ = self.forward_pass(xt=self.onehot_idx(x0))
+            rand_idx = np.random.choice(np.arange(0, self.K), p=pt.flatten('C'))
             x0 = rand_idx
             s += self.i2c[x0]
         print(s)
@@ -175,13 +224,13 @@ class RNN:
 
 def main():
     np.random.seed(2)
-    rnn = RNN(save=False, verbose=True, seq_length=5)
+    rnn = RNN(save=False, verbose=True, m=5, seq_length=5)
     rnn.prepare_data()
     rnn.init_model()
     rnn.build_mapping()
-    # h, p, a, o = rnn.forward_pass(xt=rnn.x_chars, yt=rnn.y_chars)
-    rnn.synthesize('q', n=10)
-    # rnn.adagrad(xt=rnn.x_chars, yt=rnn.y_chars)
+    # h, p, a, o, l = rnn.forward_pass(xt=rnn.x_chars, yt=rnn.y_chars)
+    rnn.synthesize('2', n=10)
+    rnn.adagrad(xt=rnn.x_chars, yt=rnn.y_chars)
 
 
 if __name__ == '__main__':
